f6a30374614863ecb4e3f4ae1b7342aee45a227c
Eldar Pikunov (EPAM) <Eldar_Pikunov@epam.com>
2021-06-11 16:09:00 +0300

    Initial Commit

---
+++ TestSVG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSVG
+//
+//  Created by Eldar Pikunov on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
